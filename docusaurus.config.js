dd2240b39c09757cc6c88206e04cb1bebd6b9713
Antony Matsoukas <amatsoukas@rapiddot.com>
2025-09-28 14:36:42 +0300

    Fix for lastmod time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,8 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          routeBasePath: '/'
+          routeBasePath: '/',
+          showLastUpdateTime: true,
         },
         blog: false,
         theme: {
